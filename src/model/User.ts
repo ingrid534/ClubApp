1d9119a29204d030bab3e1a4751458b3c3cd4db4
peterlee42 <76502486+peterlee42@users.noreply.github.com>
2025-12-24 22:45:54 -0500

    user model interface

---
+++ src/model/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber?: string;
+  followingClubIds: string[];
+  organizedClubIds: string[];
+}
