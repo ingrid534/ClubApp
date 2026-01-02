a1d98d18bac8062051b2f49f3a0f15dd3c8fcf9f
Ingrid <195973715+ingrid534@users.noreply.github.com>
2026-01-02 11:10:04 -0600

    updated .gitignore

--- frontend/.next/dev/static/chunks/src_frontend_app_page_d5d54521.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/b5835_next_dist_5047b481._.js"
-],
-    source: "dynamic"
-});
